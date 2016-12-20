47656a6bdd401965bfe28a588c873fc3d88da62e
Nsudeep <1994sudeep@gmail.com>
2016-12-19 17:02:15 -0800

    Testing widget on Relative Layout

--- app/src/main/java/com/example/sudeep/app1/Main2Activity.java
+++ app/src/main/java/com/example/sudeep/app1/Main2Activity.java
@@ -1,13 +1,42 @@
 package com.example.sudeep.app1;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        //setContentView(R.layout.activity_main2);
+        RelativeLayout rl = new RelativeLayout(this);
+        rl.setBackgroundColor(Color.YELLOW);
+        Button rb = new Button(this);
+        rb.setText("Click");
+        EditText et = new EditText(this);
+        et.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
+        et.setWidth(px);
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+        rl.addView(rb,buttonDetails);
+        rl.addView(et);
+        setContentView(rl);
+
+
     }
 }
